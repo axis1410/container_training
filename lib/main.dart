d700fe6b5c5b3ac7d14ce111f80ed2375866dae2
axis1410 <1410axis@gmail.com>
2023-01-17 11:05:07 +0530

    First UI, Space three rows around the screen

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Containers',
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData.dark()
+          .copyWith(
+            scaffoldBackgroundColor: Colors.black26,
+          )
+          .copyWith(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Containers'),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.red,
+            child: const Text('Hello'),
+          ),
+          Container(
+            color: Colors.green,
+            child: const Text('Hello'),
+          ),
+          Container(
+            color: Colors.blue,
+            child: const Text('Hello'),
+          ),
+        ],
+      ),
+    );
+  }
+}
