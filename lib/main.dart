da83d996e06335494630dd2b3e787b92c110ff62
axis1410 <1410axis@gmail.com>
2023-01-17 11:20:33 +0530

    Second UI Complete

--- lib/main.dart
+++ lib/main.dart
@@ -29,23 +29,95 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Containers'),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            color: Colors.red,
-            child: const Text('I am red'),
-          ),
-          Container(
-            color: Colors.green,
-            child: const Text('I am green'),
-          ),
-          Container(
-            color: Colors.blue,
-            child: const Text('I am blue'),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('17/1/2023'),
+                        Text('Monday'),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    const Text('Get milk from market'),
+                    const SizedBox(height: 10)
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('17/1/2023'),
+                        Text('Monday'),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    const Text('Get milk from market'),
+                    const SizedBox(height: 10)
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: Colors.white)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 5),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('17/1/2023'),
+                        Text('Monday'),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    const Text('Get milk from market'),
+                    const SizedBox(height: 10)
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
       ),
     );
   }
